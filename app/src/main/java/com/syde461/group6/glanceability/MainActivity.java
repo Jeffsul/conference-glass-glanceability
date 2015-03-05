27999f90f951e301e0d447e0c61075c2d70c12a1
Jeffsul <jeffsul16@gmail.com>
2015-03-05 18:11:22 -0500

    Major update to glanceability app
    Still operates on test mode, untested with server.

--- app/src/main/java/com/syde461/group6/glanceability/MainActivity.java
+++ app/src/main/java/com/syde461/group6/glanceability/MainActivity.java
@@ -1,10 +1,13 @@
 package com.syde461.group6.glanceability;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
+import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -20,9 +23,13 @@ import org.json.JSONObject;
 
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainActivity extends Activity {
     private static final String TAG = "glass-conference-glanceability";
+
+    private static final boolean TEST_MODE = true;
 
     private static final String API_PATH = "http://conference-glass.herokuapp.com/";
     private static final String SEND_ACTIVATION_ACTION = "web/activate";
@@ -37,6 +44,12 @@ public class MainActivity extends Activity {
         PRIMARY, SECONDARY, TERTIARY, IMAGE, DONE
     }
 
+    private enum Layout {
+        STATIC, DYNAMIC
+    }
+
+    private Layout layoutType;
+
     private Handler handler;
 
     private TextView primaryTextView;
@@ -50,40 +63,63 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle bundle) {
         super.onCreate(bundle);
 
-        setContentView(R.layout.trial_mask);
-        primaryTextView = (TextView) findViewById(R.id.user_name);
-        secondaryTextView = (TextView) findViewById(R.id.user_employer);
-        tertiaryTextView = (TextView) findViewById(R.id.user_position);
-        imageView = (ImageView) findViewById(R.id.user_profile);
+        // Stop the display from dimming.
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+        setContentView(R.layout.fixation_rect);
 
         // For posting delayed requests:
         handler = new Handler();
 
-        showMask();
-
         sendActivation();
+    }
+
+    private void updateLayoutType() {
+        if (layoutType == null) {
+            Log.e(TAG, "Layout type not set.");
+            return;
+        }
+        int layoutRes;
+        switch (layoutType) {
+            case STATIC:
+                layoutRes = R.layout.trial_mask;
+                break;
+            case DYNAMIC:
+                layoutRes = R.layout.trial_mask_dynamic;
+                break;
+            default:
+                Log.e(TAG, "Layout type not set.");
+                return;
+        }
+        setContentView(layoutRes);
+        primaryTextView = (TextView) findViewById(R.id.user_name);
+        secondaryTextView = (TextView) findViewById(R.id.user_employer);
+        tertiaryTextView = (TextView) findViewById(R.id.user_position);
+        imageView = (ImageView) findViewById(R.id.user_profile);
     }
 
     private void showMask() {
         showLayout(makeNonWord(), makeNonWord(), makeNonWord(), null);
     }
 
-    private void showLayout(String primaryWord, String secondaryWord, String tertiaryWord, String imageUrl) {
+    private void showLayout(String primaryWord, String secondaryWord, String tertiaryWord, Bitmap image) {
         primaryTextView.setText(primaryWord);
         secondaryTextView.setText(secondaryWord);
         tertiaryTextView.setText(tertiaryWord);
-        if (imageUrl == null || imageUrl.trim().length() == 0) {
-            // Display blank default image.
+        if (image == null) {
+            imageView.setImageResource(getBlankProfile(layoutType));
+        } else {
+            imageView.setImageBitmap(image);
         }
     }
 
     private void setupTrial(Question question, final long duration, final String primaryWord,
-            final String secondaryWord, final String tertiaryWord, final String imageUrl) {
+            final String secondaryWord, final String tertiaryWord, final Bitmap image) {
         showMask();
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                showLayout(primaryWord, secondaryWord, tertiaryWord, imageUrl);
+                showLayout(primaryWord, secondaryWord, tertiaryWord, image);
                 displayTimestamp = System.currentTimeMillis();
                 handler.postDelayed(new Runnable() {
                     @Override
@@ -98,11 +134,20 @@ public class MainActivity extends Activity {
 
     /** Send activation request to the server. */
     private void sendActivation() {
+        if (TEST_MODE) {
+            layoutType = Layout.STATIC;
+            updateLayoutType();
+            setupTrial(Question.PRIMARY, 10000, "Jeffrey Sullivan", "Google", "Developer", null);
+            return;
+        }
         new ServerTask().execute(API_PATH + SEND_ACTIVATION_ACTION);
     }
 
     /** Passes stimulus timestamp back to server. */
     private void sendResult() {
+        if (TEST_MODE) {
+            return;
+        }
         JSONObject data = new JSONObject();
         try {
             data.put("display_timestamp", displayTimestamp);
@@ -162,13 +207,32 @@ public class MainActivity extends Activity {
                     // Experiment is over. Do nothing.
                     return;
                 }
+
+                if (layoutType == null) {
+                    if (resp.getString("layout_type").equals(Layout.DYNAMIC.toString())) {
+                        layoutType = Layout.DYNAMIC;
+                    } else {
+                        layoutType = Layout.STATIC;
+                    }
+                    updateLayoutType();
+                }
+
                 long duration = resp.getLong("duration");
                 String primaryWord = resp.getString("primary_word");
                 String secondaryWord = resp.getString("secondary_word");
                 String tertiaryWord = resp.getString("tertiary_word");
                 String imageUrl = resp.getString("image_url");
+                Bitmap bmp = null;
+                try {
+                    URL url = new URL(imageUrl);
+                    bmp = BitmapFactory.decodeStream(url.openConnection().getInputStream());
+                } catch (MalformedURLException e) {
+                    Log.e(TAG, "Error loading image.", e);
+                } catch (IOException e) {
+                    Log.e(TAG, "Error loading image.", e);
+                }
 
-                setupTrial(question, duration, primaryWord, secondaryWord, tertiaryWord, imageUrl);
+                setupTrial(question, duration, primaryWord, secondaryWord, tertiaryWord, bmp);
             } catch (JSONException e) {
                 Log.e(TAG, "Error parsing JSON response.", e);
             }
@@ -193,5 +257,15 @@ public class MainActivity extends Activity {
             sb.append(RANDOM_CHARS.charAt((int)(Math.random() * RANDOM_CHARS.length())));
         }
         return sb.toString();
+    }
+
+    private static int getBlankProfile(Layout layoutType) {
+        switch (layoutType) {
+            case DYNAMIC:
+                return R.drawable.blank_circle;
+            case STATIC:
+            default:
+                return R.drawable.blank_box;
+        }
     }
 }
